5d0028debb97add8bef5a72bf88c4e90464f350f
имя фамилия <107165876+veganxjihad@users.noreply.github.com>
2023-10-06 18:43:26 +0500

    step two complite

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ const program = new Command();
 
 program
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
